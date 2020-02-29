61dba7c13d2e05661b0ec926cfce20ac9438f72d
Roland Sadowski <szabla@gmail.com>
2020-02-29 17:53:57 +0000

    update to version 0.2.0

--- Formula/reframe-tool.rb
+++ Formula/reframe-tool.rb
@@ -1,16 +1,21 @@
 class ReframeTool < Formula
   desc "Tool for exploring ClojureScript re-frame codebases."
   homepage "https://github.com/rosado/reframe.nim"
-  url "https://github.com/rosado/reframe.nim/archive/0.1.0.zip"
-  sha256 "5db236c21210e3f2e48967ab1172272dd8c44ec54232fd88c0119f6511f86b5b"
+  url "https://codeload.github.com/rosado/reframe.nim/zip/0.2.0"
+  sha256 "35a2083249a2753229fd9c901515a9011c1f539161f0a954eafbe308a0892fed"
 
   depends_on "nim" => [:build, :test]
   
   def install
-    system "nimble", "build", "-y"
+    system "nimble", "build", "-d:release", "-y"
     system "mv", buildpath/"reframe", buildpath/"reframe-tool"
+    system "mv", builpath/"clojure_keyword_index", buildpath/"clojure-keyword-index"
     target = prefix/"reframe-tool/bin"
+    # reframe tool
     target.install buildpath/"reframe-tool"
     bin.install_symlink target/"reframe-tool"
+    # clojure keyword index
+    target.install buildpath/"clojure-keyword-index"
+    bin.install_symlink target/"clojure-keyword-index"
   end
 end
