8e109aed7fec6803b4bea2c9929e729b1901f989
Roland Sadowski <szabla@gmail.com>
2022-01-29 13:30:37 +0000

    reframe-tool 0.4.1

--- Formula/reframe-tool.rb
+++ Formula/reframe-tool.rb
@@ -1,8 +1,8 @@
 class ReframeTool < Formula
   desc "Tool for exploring ClojureScript re-frame codebases."
   homepage "https://github.com/rosado/reframe.nim"
-  url "https://codeload.github.com/rosado/reframe.nim/zip/0.4.0"
-  sha256 "e5fc45627ea9cd326fab1e465e3e780de7570acaa8f8da3c222bb91b340d4e24"
+  url "https://codeload.github.com/rosado/reframe.nim/zip/0.4.1"
+  sha256 "d82a7e62b3c84a76e7d2807cfb6114cfce5c6cde02804d8645fb8e7de292e654"
 
   depends_on "nim" => [:build, :test]
   
